7baae4caf5dcadbf4c801bb4a78dc8d0acd67ce5
Mathieu <mathieu.deleu@gmail.com>
2014-04-26 22:34:45 +0200

    Add features

--- Peerassignment.r
+++ Peerassignment.r
@@ -1,15 +1,14 @@
-## Put comments here that give an overall description of what your## functions do## Write a short comment describing this functionmakeCacheMatrix <- function(x = matrix()) {}## Write a short comment describing this functioncacheSolve <- function(x, ...) {        ## Return a matrix that is the inverse of 'x'} 
- Put comments here that give an overall description of what your
- ## functions do
+ ##  Function makeCacheMatrix 
+ ## This function creates a special "matrix" object that can cache its inverse. 
  ## Write a short comment describing this function
  
  makeCacheMatrix <- function(x = matrix()) {}
  
- ## Write a short comment describing this function
+ ## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
  
  cacheSolve <- function(x, ...) {        
  
- ## Return a matrix that is the inverse of 'x'} 
+ ## Return a matrix that is the inverse of 'x' 
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
